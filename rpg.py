da1d8eaab2755f334a6ea2336882195084baadc1
ZLOY <patsuckow@yandex.ru>
2023-11-03 20:11:21 +0300

    Update rpg.py

--- rpg.py
+++ rpg.py
@@ -6,31 +6,36 @@ import random
 import argparse
 
 
-def arg() -> int:
+
+def arg():
     """ Get the value of the argument --len passed to the script """
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Генератор случайных строк')
     parser.add_argument(
         '--len',
         type=int,
         default=21,
-        help='provide an integer (default: 21)'
+        help='Длина генерируемой строки (default: 21)'
     )
 
-    return parser.parse_args().len
+    return parser.parse_args()
 
 
-def random_password_generator() -> str:
+def random_password_generator(length=21) -> str:
     """ :return: random string """
     # random.choice() - extracts the required number of random characters from
     # the list.
+    # Можно использовать и 
     return ''.join([
         random.choice(
             [str(i) for i in range(10)] +
             [chr(i) for i in range(65, 91)] +
             [chr(i) for i in range(97, 123)]
-        ) for _ in range(arg())
+        ) for _ in range(length)
     ])
 
 
 if __name__ == '__main__':
-    print(random_password_generator())
+    if arg().len < 1:
+        print("Длина строки должна быть больше 0")
+    else:
+        print(random_password_generator(arg().len))
